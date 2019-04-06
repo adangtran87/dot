2d1584fff06a23240428025767961c874072cdda
Alexander Dang-Tran <alex.dangtran@gmail.com>
2019-04-06 19:40:57 +0200

    iterm2 - Adjust brackets for bash compatibility

--- iTerm2/install.sh
+++ iTerm2/install.sh
@@ -2,12 +2,12 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 PROFILE=$SCRIPTPATH/profile
 ITERM2_PROFILE=~/Library/Application\ Support/iTerm2/DynamicProfiles/profile
 
-if [[ "$OSTYPE" != "darwin"* ]]; then
-  exit
+if [ ["$OSTYPE" != "darwin"*] ]; then
+  exit 0
 fi
 
 # Remove iTerm2 profile if it exists
-if [[ -f $ITERM2_PROFILE ]]; then
+if [ [ -f $ITERM2_PROFILE ] ]; then
   echo "Removing $ITERM2_PROFILE"
   rm -f "$ITERM2_PROFILE"
 fi
