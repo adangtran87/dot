a3b73a9e7a014394639aac4aae13a43d4c8d69ec
Alexander Dang-Tran <alex.dangtran@gmail.com>
2019-04-06 12:32:50 -0500

    Add iterm dynamic profile

---
+++ iTerm2/install.sh
@@ -0,0 +1,13 @@
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+PROFILE=$SCRIPTPATH/profile
+ITERM2_PROFILE=~/Library/Application\ Support/iTerm2/DynamicProfiles/profile
+
+# Remove iTerm2 profile if it exists
+if [[ -f $ITERM2_PROFILE ]]; then
+  echo "Removing $ITERM2_PROFILE"
+  rm -f "$ITERM2_PROFILE"
+fi
+
+# Create symlink to profile
+echo "Symlinking $PROFILE $ITERM2_PROFILE"
+ln -s $PROFILE "$ITERM2_PROFILE"
